254e14219148e9be7f5772a4bfd043595ca2f508
Gaurav Singh <gs062254@gmail.com>
2024-05-30 23:18:21 +0530

    Added Indian currency format in products subtotal in cart.

--- amazon-clone/src/components/SubTotal/SubTotal.jsx
+++ amazon-clone/src/components/SubTotal/SubTotal.jsx
@@ -11,51 +11,51 @@ function SubTotal() {
   const { cart } = useProductContext();
 
   // State variable to calculate and store total price of all products added to cart.
-  const [ totalCost, setTotalCost ] = useState(0);
-  
+  const [totalCost, setTotalCost] = useState(0);
+
+  // State varible to store the proce in indian format.
+  // You can convert the numbers into currencies using the Intl.NumberFormat() method in JavaScript. For example, 14340 would be $14,340.00 (US Dollars) or ₹14,340.00 (Rupees) or €14.340,00 (Euross) and so on.
+  let indianRupee = Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR"
+  })
+
+
   // Function to calculate total cost's of products.
-  function calculateTotalCostOfProducts(){
+  function calculateTotalCostOfProducts() {
     setTotalCost(0);
-      cart.forEach((element) => {
-        console.log(element.productPrice);
-        setTotalCost((prevCost) => (
-          // Product Price is in the string format like this: "1,29,999". So, before converting this to number, we need to remove all the comma's (','). 
-          // "replaceAll()" is used to replace all occurences of comma's with an empty (''). 
-          prevCost + Number(element.productPrice.replaceAll(',',''))
-        ))
-      });
+    cart.forEach((element) => {
+      setTotalCost((prevCost) => (
+        // Product Price is in the string format like this: "1,29,999". So, before converting this to number, we need to remove all the comma's (','). 
+        // "replaceAll()" is used to replace all occurences of comma's with an empty (''). 
+        prevCost + Number(element.productPrice.replaceAll(',', ''))
+      ))
+    });
   }
 
   // We need to run 'calculateTotalCostOfProducts()' method everytime, when there is a change in the product context.
-  useEffect(function(){
+  useEffect(function () {
     calculateTotalCostOfProducts();
   }, [cart]);
 
-  
+
 
   return (
     <>
-      <CurrencyFormat renderText={function (subTotalValue) {
-        return (
-          <div className='px-2 py-2 bg-white'>
-            <h2 className='text-2xl'>Subtotal <span className='font-semibold'> {subTotalValue} </span> </h2>
-            <p className='cursor-pointer mt-2'>
-              <span className='font-medium text-gray-500'>EMI Available </span>
-              <span className='font-semibold' style={{ color: "#007185" }}>Details</span>
-            </p>
-            <button className='w-full mt-4 text-black py-1.5 bg-yellow-400 hover:bg-amber-400 rounded-2xl font-medium'>Proceed to Buy ({cart.length} Items)</button>
-            <p className='flex items-center gap-2 mt-4'>
-              <input type="checkbox" id='checkboxLabel' className='p-2 w-6 h-6' />
-              <label htmlFor="checkboxLabel" className=''>Send as a gift. Include custom message</label>
-            </p>
-          </div>
-        )
-      }}
-        decimalScale={0}
-        thousandSeparator={true}
-        prefix='₹'
-        displayType='text'
-        value={totalCost} />
+
+      <div className='px-2 py-2 bg-white'>
+        <h2 className='text-2xl'>Subtotal <span className='font-semibold'> {indianRupee.format(totalCost)} </span> </h2>
+        <p className='cursor-pointer mt-2'>
+          <span className='font-medium text-gray-500'>EMI Available </span>
+          <span className='font-semibold' style={{ color: "#007185" }}>Details</span>
+        </p>
+        <button className='w-full mt-4 text-black py-1.5 bg-yellow-400 hover:bg-amber-400 rounded-2xl font-medium'>Proceed to Buy ({cart.length} Items)</button>
+        <p className='flex items-center gap-2 mt-4'>
+          <input type="checkbox" id='checkboxLabel' className='p-2 w-6 h-6' />
+          <label htmlFor="checkboxLabel" className=''>Send as a gift. Include custom message</label>
+        </p>
+      </div>
+      
     </>
   )
 }
